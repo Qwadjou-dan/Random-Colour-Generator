65d38fe4eeb7963c3891dfb06f7c0ea24b33d120
Daniel Owusu <owusudaniel077@gmail.com>
2024-10-19 21:52:10 +0000

    A function created to generate a random number from an array

---
+++ colorGenerator.js
@@ -0,0 +1,14 @@
+
+let hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+function randomColor() {
+  return Math.floor(Math.random() * hex.length);
+}
+
+console.log(randomColor());
+
+  // for(let i = 0; i < 6; i++) {
+  //   console.log(hex[randomColor()]);
+  //   console.log(hex[randomColor()]);
+  //   console.log(hex[randomColor()]);
+  // }
